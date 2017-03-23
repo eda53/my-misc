a791b3eea9e14a45d51daae5818c694fd3a868ac
Ethan Da <ethan.da@outlook.com>
2017-03-23 15:57:40 -0400

    adjust name.

--- Linux/mydropbox.sh
+++ Linux/mydropbox.sh
@@ -4,7 +4,7 @@
 
 
 todp () {
-	FILENAME="$(date '+%y%m%d%H%M%S%N')"
+	FILENAME="$(date '+%d%m%y%H%M%S%N')"
 	tar --exclude '*.svn*' --exclude '*.git*' --exclude '*CVS*' --exclude '*.o' --exclude '*.P' -czvf ${FILENAME}.tgz $@ && \
 	zip -m --password MYPASSWORD ${FILENAME}.zip ${FILENAME}.tgz && \
 	mv ${FILENAME}.zip ~/Dropbox/
