430567f76b9b586e43541c3fed6427437a548250
Ethan Da <ethan.da@outlook.com>
2017-04-21 18:32:01 -0400

    add comment and delete test; add svn-merge

--- Linux/jenkins.sh
+++ Linux/jenkins.sh
@@ -11,19 +11,6 @@ PASSWD='eda'
 JENKENS_SERVER='http://embeddedbuilder:8080'
 CURL='curl --user '"${USER}:${PASSWD}"
 
-test () {
-curl "${JENKENS_SERVER}/job/3012UDC-J2K-10G-LITE/2/doDelete" \
-	 -X POST --user "${USER}:${PASSWD}" \
-     -H "Host: embeddedbuilder:8080" \
-     -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0" \
-     -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" \
-     -H "Accept-Language: en-US,en;q=0.5" \
-     --compressed -H "Referer: http://embeddedbuilder:8080/view/EthanView/job/3012UDC-J2K-10G-LITE/1/confirmDelete" \
-     -H "Cookie: screenResolution=1920x1080; ACEGI_SECURITY_HASHED_REMEMBER_ME_COOKIE=""ZWRhOjE0OTM3MzgxMzk4NDI6MDkxZjZkZWI0NjY1ZjdmYzhlZTE1MWVmZmQ3OGM4MDZjOWNjOTk0YjliMDY4ZmNhMjc3YTliYTlkOTU0MjhkYQ==""; JSESSIONID.f11631cc=1e8jidlv0ngrc4pg868e10x7e; screenResolution=1920x1080" \
-     -H "Connection: keep-alive" \
-     --data "json="%"7B"%"7D&Submit=Yes"
-}
-
 Usage () {
 	echo 'jenkins list   [View]'
 	echo 'jenkins export [View] [Job-Filter]'
@@ -39,7 +26,10 @@ list_jobs () {
 		JENKENS_URL="${JENKENS_SERVER}"
 	fi
 
+	# discard until main-panel is seen.
 	HTML_RET=`$CURL "${JENKENS_URL}" -X POST 2>/dev/null | sed -n '/<div id="main-panel">/{p; :loop n; p; b loop}'`
+
+	# keep only matched job/JOB-NAME/
 	echo $HTML_RET | grep -oh 'job/[^/]*/' | sort -u
 }
 
@@ -49,7 +39,10 @@ export_cfg () {
 
 	JOBS_LIST="$(list_jobs $1)"
 	for job in $JOBS_LIST; do
+		# strip job/ to get the accurate name.
 		job=$(echo $job | sed 's/job//' | sed 's/\///g')
+
+		# match filter if it has.
 		if [ -z "$2" ] || echo $job | grep -q "$2"; then
 			echo -n "Get $job config      ... "
 			if $CURL "${JENKENS_SERVER}/job/$job/config.xml"  > ${CUR_TS}/${job}_config.xml 2>/dev/null; then
@@ -66,7 +59,11 @@ list_builds () {
 	[ -z "$job" ] && Usage && return
 
 	JENKENS_URL="${JENKENS_SERVER}/job/$job/"
+
+	# keep only the first /job/JOB-NAME/ (delet the second /job/JOB-NAME/ April.)
 	HTML_RET=`$CURL "${JENKENS_URL}" 2>/dev/null | grep -oh "/job/$job/[0-9]\+/\">[^/]\+/a>" | sed '0~2d'`
+
+	# strip the href string
 	echo "$HTML_RET" | sed 's/<\/a>//g' | sed 's/">/\t/g' | sort -u
 }
 
